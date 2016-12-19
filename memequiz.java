42152699acadb92bf737c4482e55e86d44e266d6
drapplebiscuit <pbdrummerboy@icloud.com>
2016-12-18 18:14:58 -0700

    Update and rename meme quiz.java to memequiz.java

---
+++ memequiz.java
@@ -0,0 +1,18 @@
+class quiz {
+
+public static void main (string[] args) {
+ test();
+}//end main
+
+ 
+ 
+ 
+ public static void test() {
+  systemout.println(kappa);
+ }//end test
+
+ 
+ 
+ 
+ 
+}//end class
