09e004ad8431c8a5a50c056e8a79d7d2d7cef58f
drapplebiscuit <pbdrummerboy@icloud.com>
2016-12-18 18:23:43 -0700

    Update memequiz.java

--- memequiz.java
+++ memequiz.java
@@ -1,4 +1,13 @@
 class quiz {
+ 
+ //app promps user with a series of multi choice questions (4)
+ //the app continues to keep track of users answers 
+ //variables change value based on user choice and will fall in to predetermined paths based on input
+ 
+ 
+ //the app prints out a meme based on user choices and a short explanation
+ // tbt
+ 
 
 public static void main (string[] args) {
  test();
